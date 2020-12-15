d1a80c5d0ab69652dd4a3fd87b2458a97a2445b2
John-G-Thomas <johnthomasg0215@gmail.com>
2020-12-15 16:29:08 -0500

    feature/added_the_final_endpoints_and_s3_bucket_framework_to_upload_and_downlaod_files

--- app/routes.py
+++ app/routes.py
@@ -18,3 +18,43 @@ async def get_url():
         url_without_password = con.engine.url.__repr__()
 
         return {'url': url_without_password} 
+
+@router.get("/case")
+async def update_case():
+        return print("database works")
+async def get_case():
+
+    with engine.connect() as con:
+        url_without_password = con.engine.url.__repr__()
+
+        return {'case': url_without_password} 
+
+@router.get("/judge")
+async def update_judge():
+        return print("database works")
+async def get_judge():
+
+    with engine.connect() as con:
+        url_without_password = con.engine.url.__repr__()
+
+        return {'url': url_without_password} 
+
+@router.get("/book_mark_judge")
+async def update_bookmark_judge():
+        return print("database works")
+async def get_book_mark_judge():
+
+    with engine.connect() as con:
+        url_without_password = con.engine.url.__repr__()
+
+        return {'user': url_without_password}
+
+@router.get("/book_mark_case")
+async def update_bookmark_case():
+        return print("database works")
+async def get_book_mark_case():
+
+    with engine.connect() as con:
+        url_without_password = con.engine.url.__repr__()
+
+        return {'user': url_without_password} 
