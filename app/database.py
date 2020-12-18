459f3813eff344874bcb27094ccb5c5a1fc95591
John-G-Thomas <johnthomasg0215@gmail.com>
2020-12-18 12:34:44 -0500

    debugging

--- app/database.py
+++ app/database.py
@@ -1,52 +1,31 @@
 """This defines our database connection using SQLAlchemy."""
 
 import os
-
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
 from dotenv import load_dotenv
 import psycopg2
 
-# to load the credentials from .env file
-load_dotenv()
 
-rds_username = os.getenv('rds_username')
-rds_password = os.getenv('rds_password')
-rds_endpoint = 'asylumdb.cxip2v9lysxm.us-east-2.rds.amazonaws.com'
-port = '5432'
-database_name = 'postgres'
+# # to load the credentials from .env file
+# load_dotenv()
 
-class Engine:
-    def __init__(self):
-        try:
-            self.connection = psycopg2.connect(database="postgres",
-                                                user=rds_username,
-                                                password=rds_password,
-                                                host=rds_endpoint,
-                                                port=port)
-            self.connection.autocommit = True
-            self.cursor = self.connection.cursor()
-        except:
-            print("Cannot connect to Database")
-    def create_table(self):
-        create_table_command = "CREATE TABLE placeholder(id serial PRIMARY KEY, name varchar(100), age integer NOT NULL)"
-        self.cursor.execute(create_table_command)
+# rds_username = os.getenv('rds_username')
+# rds_password = os.getenv('rds_password')
+# rds_endpoint = 'asylumdb.cxip2v9lysxm.us-east-2.rds.amazonaws.com'
+# port = '5432'
+# database_name = 'hrfasyluma'
 
+# # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# RDS_DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}'.format(rds_username, rds_password, rds_endpoint, port, database_name)
 
-# to check the connection 
-if __name__ == "__main__":
-    base = Engine()
+# # to connect to the database
+# engine = create_engine(RDS_DATABASE_URL)
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-RDS_DATABASE_URL = 'postgresql://{}:{}@{}:{}/postgres'.format(
-                            rds_username, rds_password, rds_endpoint, port, database_name)
-
-# to connect to the database
-engine = create_engine(RDS_DATABASE_URL)
-
-# To be able to talk to the db, make a session 
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
+# # To be able to talk to the db, make a session 
+# session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
 
 Base = declarative_base()
 
--- app/routes.py
+++ app/routes.py
@@ -3,58 +3,32 @@
 from fastapi import APIRouter
 # from app.schemas import 
 #from app.database import session_local, engine
-from app.database import session_local, engine
-from app.models import Case, User, Judge, BookMarkJudge, BookMarkCase
+#from .database import session_local, engine
+from .models import Case, User, Judge, BookMarkJudge, BookMarkCase
 
 router = APIRouter()
 
 
 @router.get("/info")
 async def update_data():
-        return print("database works")
+        return print("route works")
 async def get_url():
-
     with engine.connect() as con:
         url_without_password = con.engine.url.__repr__()
-
         return {'url': url_without_password} 
 
 @router.get("/case")
 async def update_case():
-        return print("database works")
+        return print("route works")
 async def get_case():
-
     with engine.connect() as con:
-        url_without_password = con.engine.url.__repr__()
-
-        return {'case': url_without_password} 
+        case_url = con.engine.url.__repr__()
+        return {'case_url':case_url } 
 
 @router.get("/judge")
 async def update_judge():
-        return print("database works")
+        return print("route works")
 async def get_judge():
-
     with engine.connect() as con:
-        url_without_password = con.engine.url.__repr__()
-
-        return {'url': url_without_password} 
-
-@router.get("/book_mark_judge")
-async def update_bookmark_judge():
-        return print("database works")
-async def get_book_mark_judge():
-
-    with engine.connect() as con:
-        url_without_password = con.engine.url.__repr__()
-
-        return {'user': url_without_password}
-
-@router.get("/book_mark_case")
-async def update_bookmark_case():
-        return print("database works")
-async def get_book_mark_case():
-
-    with engine.connect() as con:
-        url_without_password = con.engine.url.__repr__()
-
-        return {'user': url_without_password} 
+        name = con.engine.url.__repr__()
+        return {'judge': name} 
