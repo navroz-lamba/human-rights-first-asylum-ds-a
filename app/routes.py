4464d19d7fe7138793664313bf2813742d7c01e4
navroz-lamba <lamba-navroz@lambdastudents.com>
2020-12-18 15:53:22 -0500

    Established a working connection with AWS RDS

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,15 @@ from fastapi import APIRouter, File, UploadFile
 from app.models import Case, User, Judge, Base
 from app import models
 from sqlalchemy import *
-#from .database import Base, MetaData, engine
+from .database import Base, engine
 from .models import Judge, Case
 router = APIRouter()
-from app.database import Base
 
 @router.get("/case")
 async def update_case():
         return print("database works")
 async def get_case():
-    with Base.connect() as con:
+    with engine.connect() as con:
         case_without_password = con.engine.case.__repr__()
         return {'case': case_without_password} 
 
@@ -21,7 +20,7 @@ async def get_case():
 async def update_judge():
         return print("database works")
 async def get_judge():
-    with Base.connect() as con:
+    with engine.connect() as con:
         judge_without_password = con.engine.judge.__repr__()
         return {'judge': judge_without_password} 
 
