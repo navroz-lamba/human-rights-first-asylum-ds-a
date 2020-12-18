448985268f77a0be158fbc211fa0da0169ade191
John-G-Thomas <johnthomasg0215@gmail.com>
2020-12-18 17:19:16 -0500

    changes to Post request

--- app/upload_file.py
+++ app/upload_file.py
@@ -31,39 +31,39 @@ def upload_file(file_name, bucket, object_name=None):
         logging.error(e)
         return "didn't upload to S3"
     return "Uploaded :)"
-
+# parse case_url and scrape relivent data off of it
+def case_urls(str):
+    # case url data for web to no name of document on S3 buckets to view
+    case_url = str[:-4]
+    hearing_date = ""
+    decision_date = ""
+    department = ""
+    refugee = ""
+    return case_url,hearing_date,decision_date,department,refugee
 # file uploaders
 @router.post("/upload/pdf")
 async def pdf(file: UploadFile = File(...)):
     if len(file.filename) >= 1:
-        # parse case_url and scrape relivent data off of it
-        def case_url(str):
-            # case url data for web to no name of document on S3 buckets to view
-            case_url = str[:-4]
-            hearing_date = ""
-            decision_date = ""
-            department = ""
-            refugee = ""
-            return case_url,hearing_date,decision_date,department,refugee
         
         # add these varibles to table's scrapes all the data we need from initional upload
-        case_url,hearing_date,decision_date,department,refugee = case_url(file.filename)
+        case_url,hearing_date,decision_date,department,refugee = case_urls(file.filename)
         
         # Uploads File
-        upload = upload_file(file_name, bucket, object_name=None)
+        upload = upload_file(file, bucket='hrf-asylum-dsa-documents', object_name=case_url)
     return {"filename": file.filename,
             "case_url" : case_url,
-            "dates" :  (hearing_date,decision_date),
+            "hearing_date" : hearing_date,
+            "decision_date" : decision_date,
             "department": department,
             "refugee": refugee,
             "Test": upload}
 
+# deals with data from csv
+def csv_data(df):
+    return ""
+
 @router.post("/upload/file")
 async def not_pdf(file: UploadFile = File(...)):
-    # deals with data from csv
-    df = pf.read_csv(file)
-    def csv_data(df):
-        return
     # add these varibles to table's
     varibles = csv_data(df)
     return {"filename": file.filename}
